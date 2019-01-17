9bc5bcd731c4e4de8cc4b25b9e14d1678c4e976d
aitorgit <tugasmec@gmail.com>
2019-01-17 17:27:48 +0100

    black background and searchbar

--- app/src/main/java/com/example/aitor/androidpractice1_periodictable/MainActivity.java
+++ app/src/main/java/com/example/aitor/androidpractice1_periodictable/MainActivity.java
@@ -1,11 +1,17 @@
 package com.example.aitor.androidpractice1_periodictable;
 
+import android.app.SearchManager;
 import android.content.DialogInterface;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,6 +45,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         sulfurButton = (ImageButton) findViewById(R.id.imgBtnSulfur);
         sulfurButton.setOnClickListener(this);
     }
+
+    /// FUMADON LETSGOOOOOOOOOOOOOOOOO
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        if (searchItem != null) {
+            SearchView searchView = (SearchView)searchItem.getActionView();
+            searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+                @Override
+                public boolean onClose() {
+                    //some operation
+                    return false;
+                }
+            });
+            searchView.setOnSearchClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //some operation
+                }
+            });
+            EditText searchPlate = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+            searchPlate.setHint("Search");
+            View searchPlateView = searchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
+            searchPlateView.setBackgroundColor(ContextCompat.getColor(this, android.R.color.transparent));
+            // use this method for search process
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    // use this method when query submitted
+                    // Toast.makeText(context, query, Toast.LENGTH_SHORT).show();  PEAEEEEEEEE
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    // use this method for auto complete search process
+                    return false;
+                }
+            });
+            SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+/*
+    @Override
+    public void onBackPressed() {
+        if (!searchView.isIconified()) {
+            searchView.setIconified(true);
+            findViewById(R.id.default_title).setVisibility(View.VISIBLE);
+        } else {
+            super.onBackPressed();
+        }
+    }*/
+
+    /// LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
 
 
     @Override
