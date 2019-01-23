affb7424748474fc602c6214ecdcbe86db5fdba7
aitor gallardo muñoz <gmaitor@outlook.com>
2019-01-23 03:40:02 +0100

    Searching correctly

--- app/src/main/java/com/example/aitor/androidpractice1_periodictable/Main2ListActivity.java
+++ app/src/main/java/com/example/aitor/androidpractice1_periodictable/Main2ListActivity.java
@@ -1,24 +1,36 @@
 package com.example.aitor.androidpractice1_periodictable;
 
 import android.app.SearchManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Parcelable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SearchView;
+import android.text.Layout;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Main2ListActivity extends AppCompatActivity {
-    Bundle mainActivityInfo;
+
     Intent intent;
     ListView listViewOfElements;
+    ArrayList<ChemicalElement> listOfElements;
+    CElementAdapter allItemsAdapter;
+    SearchView searchView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,18 +38,18 @@ public class Main2ListActivity extends AppCompatActivity {
 
         intent = new Intent(this, MainActivity.class);
         // mainActivityInfo = getIntent().getExtras();
-        ArrayList<ChemicalElement> listOfElements = getIntent().getParcelableArrayListExtra("Elements");
+        listOfElements = getIntent().getParcelableArrayListExtra("Elements");
 
 
-        CElementAdapter adaptador =
-                new CElementAdapter(this, listOfElements);
+                allItemsAdapter =   new CElementAdapter(this, listOfElements);
 
         listViewOfElements = (ListView)findViewById(R.id.simple_list_item);
 
         if(listViewOfElements != null){
-            listViewOfElements.setAdapter(adaptador);
+            listViewOfElements.setAdapter(allItemsAdapter);
         }
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -45,7 +57,7 @@ public class Main2ListActivity extends AppCompatActivity {
         final MenuItem searchItem = menu.findItem(R.id.action_search);
 
         if (searchItem != null) {
-            SearchView searchView = (SearchView)searchItem.getActionView();
+            searchView = (SearchView)searchItem.getActionView();
             searchView.setOnCloseListener(new SearchView.OnCloseListener() {
                 @Override
                 public boolean onClose() {
@@ -56,9 +68,9 @@ public class Main2ListActivity extends AppCompatActivity {
             searchView.setOnSearchClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //some operation
                 }
             });
+
             EditText searchPlate = (EditText) searchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
             searchPlate.setHint("Search");
             View searchPlateView = searchView.findViewById(android.support.v7.appcompat.R.id.search_plate);
@@ -68,37 +80,82 @@ public class Main2ListActivity extends AppCompatActivity {
                 @Override
                 public boolean onQueryTextSubmit(String query) {
                     // use this method when query submitted
-                    // Toast.makeText(context, query, Toast.LENGTH_SHORT).show();
+                    search(query);
                     return false;
                 }
 
                 @Override
                 public boolean onQueryTextChange(String newText) {
-                    // use this method for auto complete search process
+                    if (TextUtils.isEmpty(newText)){
+                        listViewOfElements.setAdapter(allItemsAdapter);
+                        // searchView.setIconified(true);
+                        searchView.clearFocus();
+                    } else {
+                        search(newText);
+                    }
                     return false;
+                }
+            });
+            searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+                @Override
+                public void onFocusChange(View view, boolean queryTextFocused) {
+
+//                    if (!searchView.isIconified()) {
+//                        searchView.setIconified(true);
+//                    }
+
                 }
             });
             SearchManager searchManager = (SearchManager) getSystemService(SEARCH_SERVICE);
             searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
 
         }
+//        searchView.setOnCloseListener(new SearchView.OnCloseListener() {
+//            @Override
+//            public boolean onClose() {
+//
+//                listViewOfElements.setAdapter(allItemsAdapter);
+//                return false;
+//            }
+//        });
         // BUTTON TO GO TO LISTS
 
 
 
         return super.onCreateOptionsMenu(menu);
     }
-/*
-    @Override
-    public void onBackPressed() {
-        if (!searchView.isIconified()) {
-            searchView.setIconified(true);
-            findViewById(R.id.default_title).setVisibility(View.VISIBLE);
-        } else {
-            super.onBackPressed();
+
+
+    public void hideKeyboard(View view) {
+        InputMethodManager inputMethodManager =(InputMethodManager)getSystemService(Main2ListActivity.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    public void search(String query) {
+
+        ArrayList<ChemicalElement> copyOfList = new ArrayList<ChemicalElement>();
+        boolean succeededSearch;
+
+         if(query != null){
+
+             String queryToLow = query.toLowerCase();
+
+            for(ChemicalElement element : listOfElements) {
+                succeededSearch = false;
+
+                if(element.name.toLowerCase().contains(queryToLow)){
+                    succeededSearch = true;
+                }
+//                if(element.atomicMass.contains(query)){  // MORE OPTIONS FOR SEARCH
+//
+//                }
+                if(succeededSearch){
+                    copyOfList.add(element);
+                }
+            }
         }
-    }*/
-
-    /// LETS GOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
-
+        CElementAdapter adaptador =
+                new CElementAdapter(this,copyOfList);
+        listViewOfElements.setAdapter(adaptador);
+    }
 }
